7884eec1c818a6fbd8343bd1b368538b129faa22
KeenanTrey <46493050+KeenanTrey@users.noreply.github.com>
2021-03-14 16:16:28 -0600

    www test

--- www/js/dashboard/sidebar/sideBarOptions.js
+++
@@ -1,9 +0,0 @@
- 
- import {optionsTemplate} from './sideBarTemplates.js'
- import renderToElement from '../../utils/renderToElement.js'
-
- const sideBarOptions = function(){
-      return renderToElement(optionsTemplate)
- }
- 
- export default sideBarOptions 
